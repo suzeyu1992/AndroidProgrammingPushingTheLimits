9394c8912bde43ba7f4686bbe219359663cff284
suzeyu <suzeyu_1992@163.com>
2016-10-03 23:45:47 +0800

    本地c代码实现activity(目前存在无法找到mainbug)

---
+++ JNIDemo/app/src/main/obj/local/armeabi/objs/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,5 @@
+obj/local/armeabi/objs/android_native_app_glue/android_native_app_glue.o: \
+ /Users/suzeyu/Library/Android/sdk/ndk-bundle/sources/android/native_app_glue/android_native_app_glue.c \
+ /Users/suzeyu/Library/Android/sdk/ndk-bundle/sources/android/native_app_glue/android_native_app_glue.h
+
+/Users/suzeyu/Library/Android/sdk/ndk-bundle/sources/android/native_app_glue/android_native_app_glue.h:
