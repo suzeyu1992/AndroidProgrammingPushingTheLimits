e7456b8db22abf0b579e5995c970783e1b6a6ce7
suzeyu <suzeyu_1992@163.com>
2016-10-09 10:15:16 +0800

    WiFi-Direct模式设备发布服务代码

--- Network/app/src/main/java/com/szysky/note/network/MainActivity.java
+++ Network/app/src/main/java/com/szysky/note/network/MainActivity.java
@@ -2,16 +2,26 @@ package com.szysky.note.network;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String SERVICE_NAME = "测试服务";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +31,67 @@ public class MainActivity extends AppCompatActivity {
         // 发送定时log达到轮询效果
 //        setRepeatSend();
 
-        announceService();
+        // 设置本地一个wifi服务
+//        announceService();
+
+        // 发现本地网络的wifi服务
+//        discoverService();
+
+
+
+    }
+
+
+    public void initWifiDirect(){
+        IntentFilter intentFilter = new IntentFilter(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        BroadcastReceiver myReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.e(TAG, "接收到wifi-direct的变化" );
+            }
+        };
+
+        registerReceiver(myReceiver, intentFilter);
+
+
+    }
+    /**
+     *  在设备进行发布服务, 并初始化 Wi-Fi Direct渠道
+     */
+    private void announceWiFiDirectService(){
+        WifiP2pManager wifiP2pManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        HandlerThread handlerThread = new HandlerThread(TAG);
+        handlerThread.start();
+        final Looper mWFDLooper = handlerThread.getLooper();
+        WifiP2pManager.Channel mChannel = wifiP2pManager.initialize(getApplicationContext(), mWFDLooper, new WifiP2pManager.ChannelListener() {
+            @Override
+            public void onChannelDisconnected() {
+                Log.e(TAG, "通道回调断开");
+                mWFDLooper.quit();
+            }
+        });
+
+        HashMap<String, String> txtRecords = new HashMap<>();
+        WifiP2pDnsSdServiceInfo mServiceInfo = WifiP2pDnsSdServiceInfo.newInstance(SERVICE_NAME, "_http._tcp", txtRecords);
+
+        wifiP2pManager.addLocalService(mChannel, mServiceInfo, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d(TAG, "Service announcing !");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.d(TAG, "Service announcing failed : "+reason);
+            }
+        });
 
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        discoverService();
 
-    }
 
     /**
      * 设置定时轮序检测功能
